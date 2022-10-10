ac2965e4ea3e58f784293ba08d11b3ca52da8d9c
theguxz06 <98673866+theguxz06@users.noreply.github.com>
2022-10-10 03:03:27 -0500

    6
    6

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -7,11 +7,11 @@
     $usuario = $_POST['usuario'];
     $contrasena = $_POST['contrasena'];
 
-    $query = "INSERT INTO usuarios(nombre_completo, Correo, Usuario, contrasna) 
+    $query = "INSERT INTO usuarios2(nombre_completo, Correo, Usuario, contrasna) 
               VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
 
     //verifocar que el correo no se repita
-    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo= '$correo' ");
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios2 WHERE Correo= '$correo' ");
 
     if(mysqli_num_rows($verificar_correo) >0){
         echo '
@@ -23,7 +23,7 @@
         exit();
     }
       //verifocar que el nombre de usuario no se repita
-    $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario= '$usuario' ");
+    $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios2 WHERE Usuario= '$usuario' ");
      if(mysqli_num_rows($verificar_usuario) >0){
         echo '
             <script>
@@ -33,7 +33,6 @@
         ';
         exit();
     }
-
 
     $ejecutar = mysqli_query($conexion, $query);
 
