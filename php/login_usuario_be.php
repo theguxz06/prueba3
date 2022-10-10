82fa7f8c0bf00c2fc7635c86721b7eb50c8fd9da
theguxz06 <98673866+theguxz06@users.noreply.github.com>
2022-10-10 03:03:04 -0500

    5
    5

--- php/login_usuario_be.php
+++ php/login_usuario_be.php
@@ -4,7 +4,7 @@ include 'conexion_be.php';
     $correo = $_POST['correo'];
     $contrasena = $_POST['contrasena'];
 
-    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios
+    $validar_login = mysqli_query($conexion, "SELECT * FROM usuarios2
      WHERE correo='$correo'  and contrasna='$contrasena'");
 
     if(mysqli_num_rows($validar_login) > 0){
